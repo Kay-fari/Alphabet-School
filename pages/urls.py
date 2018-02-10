83f5cfd46ea4c2a46f7384bb20624b5f3d9f47ab
Gilles Ashley <xylaray37@gmail.com>
2018-02-10 16:54:48 +0000

    welcome page stuff

--- pages/urls.py
+++ pages/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     re_path(r'^about$', views.AboutView.as_view(), name='about'),
     re_path(r'^inquiry$', views.InquiryView.as_view(), name='inquiry'),
     re_path(r'^faq$', views.FAQView.as_view(), name='faq'),
-    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^index$', views.IndexView.as_view(), name='index'),
+    re_path(r'^$', views.WelcomePageView.as_view(), name='welcome'),
 ]
