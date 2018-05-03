3c423305776af1dd86dd109bd1e5af711d0a7301
Gilles Ashley <xylaray37@gmail.com>
2018-05-03 19:05:19 +0100

    resolved url errors

--- pages/urls.py
+++ pages/urls.py
@@ -1,16 +1,16 @@
-from django.urls import re_path
-from . import views
+from django.urls import path
+from pages import views
 
 
 urlpatterns = [
-	re_path(r'^curriculum$', views.CurriculumView.as_view(), name='curriculum'),
-	re_path(r'^gallery$', views.GalleryView.as_view(), name='gallery'),
-	re_path(r'^sampleDay$', views.SampleDayView.as_view(), name='sampleDay'),
-    re_path(r'^register/$', views.register, name='register'),
-    re_path(r'^contact$', views.contact, name='contact'),
-    re_path(r'^about$', views.AboutView.as_view(), name='about'),
-    re_path(r'^inquiry$', views.InquiryView.as_view(), name='inquiry'),
-    re_path(r'^faq$', views.FAQView.as_view(), name='faq'),
-    re_path(r'^index$', views.IndexView.as_view(), name='index'),
-    re_path(r'^$', views.WelcomePageView.as_view(), name='welcome'),
+    path('curriculum/', views.CurriculumView.as_view(), name='curriculum'),
+    path('gallery/', views.GalleryView.as_view(), name='gallery'),
+    path('sampleDay/', views.SampleDayView.as_view(), name='sampleDay'),
+    path('register/', views.register, name='register'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('inquiry/', views.InquiryView.as_view(), name='inquiry'),
+    path('faq/', views.FAQView.as_view(), name='faq'),
+    path('index/', views.IndexView.as_view(), name='index'),
+    path('', views.WelcomePageView.as_view(), name='welcome'),
 ]
